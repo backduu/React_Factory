983186914f696ca750fa8dacf121c66566ab8b10
backduu <power4290@gmail.com>
2025-08-25 14:21:42 +0900

    #5. aboutPage, ContactPage 구현

--- Q2.Bookstore/src/pages/AboutPage.tsx
+++ Q2.Bookstore/src/pages/AboutPage.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import React, { useEffect } from 'react';
 
 const AboutPage: React.FC = () => {
+    {/* useEffect의 애니메이션 효과 */}
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry)=> {
+                if(entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                } else {
+                    entry.target.classList.remove('show');
+                }
+            });
+        });
+
+        const elements = document.querySelectorAll('.hidden');
+        elements.forEach((el) => observer.observe(el));
+    }, []);
+
     return (
-        <div>
-            about site..
-        </div>
-    );
+    <>
+        <Container>
+            <Row className="text-center">
+                <Col md={6} className='header_left hidden'>
+                    <header className='header_left_intro'>[  바트심슨  ]</header>
+                    <header className='header_left_introduce'>Hello,</header>
+                    <header className='header_left_introduce'>this is bart simpson</header>
+                    <div className='header_left_introduce_body'>
+                        <span className='header_left_introduce_body'>lol lol lol</span><br/>
+                        <span className='header_left_introduce_body'>임시로 적어넣은 공간이니 나중에 필요할 때 써먹어보세요.</span>
+                    </div>
+                </Col>
+                
+                <Col me={6}>
+                    <img src='member.jpg' width={"70%"} height={"auto"} alt="대표 이미지"/>
+                </Col>
+            </Row>
+
+            {/* introduce */}
+
+
+        </Container>
+    </>
+    )
 }
 
 export default AboutPage;
--- Q2.Bookstore/src/pages/ListPage.tsx
+++ Q2.Bookstore/src/pages/ListPage.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
+import axios from 'axios';
+import bookdata from '../data/Bookdata';
+import { Container, Row } from 'react-bootstrap';
+import BookItem from '../components/BookItem';
 
 const ListPage: React.FC = () => {
+    const [books, setBooks] = useState(bookdata);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(()=>{
+        if(!loaded) 
+        {
+            axios.get('/books.json')
+            .then((result) => {
+                console.log("result 데이터: ");
+                console.log(result.data);
+                console.log("books: ");
+                console.log(books);
+
+                let copy = [...books, ...result.data];
+                setBooks(copy);
+                setLoaded(true);
+
+                console.log("copy = [...books, ...result.data] : ");
+                console.log(copy);
+            })
+            .catch(()=>{
+                console.log('fail');
+            });
+        }
+    }, [loaded, books])
     return (
         <div>
-            list page
+            <Container>
+                <div className='project_header_container'>
+                    <h1 className='project_header'>도서 리스트</h1>
+                    <div className='hr'></div>
+                </div>
+                <div>
+                    <Row className="text-center">
+                        {books.map((item, i) => (
+                            <BookItem key={i} book={item}></BookItem>
+                        ))}
+                    </Row>
+                </div>
+            </Container>
         </div>
     );
 }
